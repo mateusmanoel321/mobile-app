4afbcd5723eb0c7d14fe66a55165a3417dc7d3a3
Mateus <mateus.manoel1998@gmail.com>
2023-09-12 20:13:06 -0400

    feat: start app

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>teste</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
