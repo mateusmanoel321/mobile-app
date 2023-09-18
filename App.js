714eb62c1e08cc530d76ed84e24100c7a385e930
Jaqueline Bravin <jaqbravin@hotmail.com>
2023-09-17 20:19:40 -0400

    Adicionando textos no front

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
 
 const HomeScreen = () => {
   return (
@@ -34,8 +36,33 @@ const HomeScreen = () => {
           <Text style={styles.bookNumber}>2</Text>
         </View>
         <View style={[styles.book, styles.marginLeft27]}>
-          {/* Adicione a terceira imagem aqui */}
+          {/* Adicione a terceira imagem aqui */};
           <Text style={styles.bookNumber}>3</Text>
+        </View>
+      </View>
+      <View>
+        <Text>Populares</Text>
+        <Text>Em breve</Text>
+        <Text>Premiados</Text>
+        <Text>Brasileiros</Text>
+        <View style={styles.bookContainer}>
+          <View style={[styles.book, styles.marginLeft34]}>
+            <Image
+              source={require("./assets/images/images.jpg")}
+              style={styles.bookImage}
+            />
+            <Text style={styles.bookNumber}>1</Text>
+          </View>
+          <View style={[styles.book, styles.marginLeft31]}>
+            <Image
+              source={require("./assets/images/download.jpg")}
+              style={styles.bookImage}
+            />
+            <Text style={styles.bookNumber}>2</Text>
+          </View>
+          <View style={[styles.book, styles.marginLeft27]}>
+            <Text style={styles.bookNumber}>3</Text>
+          </View>
         </View>
       </View>
     </View>
